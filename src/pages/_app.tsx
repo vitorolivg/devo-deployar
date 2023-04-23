d20503c4877428b186b64895fa3966f7237c020f
Vitor Gomes <vitorgomes@Vitors-MacBook-Pro.local>
2023-04-23 12:25:27 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { Inter, DM_Sans } from "next/font/google";
+import { extendTheme } from "@chakra-ui/react";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
+
+const theme = extendTheme({
+  fonts: {
+    inter: inter.style.fontFamily,
+    dmSans: dmSans.style.fontFamily,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
